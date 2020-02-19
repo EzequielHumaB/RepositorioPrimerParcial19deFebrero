9ca29f66983c8f556eca70982d7fead4795bb431
ezequiel huma brito <ezequielhuma@hotmail.com>
2020-02-19 14:42:51 -0400

    Instalando Entity Framework Core.sqlite y Entity framework core.tools, tambien la eliminicacion Counter y Fecth Data.

---
+++ BlazorPrimerParcial19DeFebero2020/Data/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace BlazorPrimerParcial19DeFebero2020.Data
+{
+    public class Contexto : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder.UseSqlite("Data Source = Database/PrimerParcial19DeFebrero2020Db"));
+        }
+    }
+}
